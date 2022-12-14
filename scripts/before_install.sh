14d534e7c83d162f7330ffcb7fb1bc2af6693994
Sparsh-Agrawal <44096686+Sparsh-Agrawal@users.noreply.github.com>
2022-12-14 18:09:35 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,10 +4,4 @@ cd /var/tmp/testing/
 
 docker build -t spagrawa/apacheci:latest .
 
-docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-
-echo ${DOCKER_USERNAME}
-
-whoami
-
 docker push spagrawa/apacheci:latest
