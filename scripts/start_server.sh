16f9fd542bbe5b3240189366261c386ef960a3cb
Sparsh-Agrawal <44096686+Sparsh-Agrawal@users.noreply.github.com>
2022-12-14 18:14:48 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,10 @@ cd /var/tmp/testing/
 
 alias kubectl="minikube kubectl --"
 
+whoami
+
+kubectl get pods
+
 kubectl delete deployment test
 kubectl create deployment test --image=spagrawa/apacheci:latest
 kubectl delete svc test
