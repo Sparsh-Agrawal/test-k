c549bca83aac4d8d4223139a2cb996029e65dd95
Sparsh-Agrawal <44096686+Sparsh-Agrawal@users.noreply.github.com>
2022-12-14 18:43:57 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -11,6 +11,6 @@ whoami
 minikube kubectl -- get po -A
 
 kubectl delete deployment test
-kubectl create deployment test --image=spagrawa/apacheci:latest
+kubectl create -f deploy.yml
 kubectl delete svc test
 kubectl create -f service.yml
