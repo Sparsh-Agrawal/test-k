26493dea2390cef78cf1bedbb3447e38b0610dc5
Sparsh-Agrawal <44096686+Sparsh-Agrawal@users.noreply.github.com>
2022-12-14 16:39:31 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+cd /var/tmp/testing/
+
+kubectl delete deployment test
+kubectl create deployment test --image=spagrawa/apacheci:latest
+kubectl delete svc test
+kubectl create -f service.yml
